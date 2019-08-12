ad6dd12e0e4e4310e00a0d40968eea884d6f7a96
Victor <angvictor91@gmail.com>
2019-08-12 16:43:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chattingapp/register/RegisterActivity.java
@@ -0,0 +1,75 @@
+package com.example.chattingapp.register;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.example.chattingapp.R;
+import com.example.chattingapp.databinding.ActivityRegisterBinding;
+import com.example.chattingapp.login.LoginActivity;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityRegisterBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+        initListener();
+    }
+
+    private void initListener() {
+        mBinding.btnGotoLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mBinding.btnGotoLogin) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onBackPressed() {
+        showAlertDialog();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to quit the application")
+                .setTitle("Quit")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/chattingapp/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.chattingapp.splashscreen;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chattingapp.R;
+import com.example.chattingapp.login.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        setSplashScreen();
+    }
+
+    private void setSplashScreen() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
